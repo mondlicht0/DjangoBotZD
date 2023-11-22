143259aa11bc5042c2c981f53444db0eff72d353
mondlicht0 <damirzhakhan.j@gmail.com>
2023-11-22 12:34:58 +0600

    Update run_telegram_bot.py

--- TelegramBotDjango/djangobot/shop/management/commands/run_telegram_bot.py
+++ TelegramBotDjango/djangobot/shop/management/commands/run_telegram_bot.py
@@ -3,12 +3,16 @@ from shop.models import Product, Video
 import telebot
 
 bot = telebot.TeleBot("6948290779:AAGJgiMOwcr6qxp7Tod1YHnlb_S2DlZPbpQ")
-video_list = {}
-
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Привет, я бот который хранит твои сохранненые видео!\n Используй команды /add и /videos")
+    bot.send_message(message.chat.id, "Hello World")
+
+@bot.message_handler(commands=['products'])
+def products(message):
+    products = Product.objects.all()
+    for product in products:
+        bot.send_message(message.chat.id, f"Name: {product.name}, Price: {product.price}")
 
 @bot.message_handler(commands=['videos'])
 def videos(message):
@@ -17,16 +21,11 @@ def videos(message):
         bot.send_message(message.chat.id, f"Name: {video.name}, URL: {video.url}")
 
 @bot.message_handler(commands=['add'])
-def add_video(message):
+def add(message):
     bot.reply_to(message, f"Напишите название видео")
-
-    title_handler()
 
     new_product = Product.objects.create(name=product_name, price=product_price)
 
-def title_handler(message):
-    title = message.text
-    bot.message.reply_text(f"Спасибо, вставьте пожалуйста URL видео")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
