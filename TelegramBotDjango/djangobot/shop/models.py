7dd19d18e46418b44dd37fddcb48b6fdf8212dd7
mondlicht0 <damirzhakhan.j@gmail.com>
2023-11-29 13:52:25 +0600

    fix

--- TelegramBotDjango/djangobot/shop/models.py
+++ TelegramBotDjango/djangobot/shop/models.py
@@ -7,3 +7,6 @@ class Product(models.Model):
 class Video(models.Model):
     name = models.CharField(max_length=255)
     url = models.CharField(max_length=255)
+
+    def get_fields(self):
+        return f"{self.name}, {self.url}"
--- TelegramBotDjango/djangobot/shop/tests.py
+++ TelegramBotDjango/djangobot/shop/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Video
 
-# Create your tests here.
+class VideoTestCase(TestCase):
+    def test_fields(self):
+        video1 = Video.objects.create(name="psy gagnam", url="takoi-to")
+        self.assertEqual(video1.get_fields(), "psy gagnam, takoi-to")
+
+    def test_model_name(self):
+        video1 = Video.objects.create(name="tigr", url="krutoi")
+        self.assertEqual(video1.name, "tigr")
+
+    def test_model_url(self):
+        video1 = Video.objects.create(name="tigr", url="krutoi")
+        self.assertEqual(video1.url, "krutoi")
